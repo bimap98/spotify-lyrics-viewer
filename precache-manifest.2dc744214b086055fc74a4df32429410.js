4be34dd211a97dbf6103b57f2d016d0708591c75
brentvollebregt <volleys2468@gmail.com>
2019-11-04 03:28:33 +0000

    Deploying to gh-pages from master 0df81af2e42396f1fa24afdee98726446190412e

---
+++ precache-manifest.2dc744214b086055fc74a4df32429410.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10750db8af08db28b773002c4050e32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61d7dbf031852105075d",
+    "url": "/static/css/main.8c6f4a6f.chunk.css"
+  },
+  {
+    "revision": "3d980f10f485b073fcc8",
+    "url": "/static/js/2.373bcd25.chunk.js"
+  },
+  {
+    "revision": "61d7dbf031852105075d",
+    "url": "/static/js/main.54223df0.chunk.js"
+  },
+  {
+    "revision": "f4baa4d52a74eb3cd183",
+    "url": "/static/js/runtime~main.20d38192.js"
+  },
+  {
+    "revision": "ba11245090a54e6667f5fddc91ad98d7",
+    "url": "/static/media/banner.ba112450.png"
+  },
+  {
+    "revision": "d154494c9913b044d7a2abb63ee91828",
+    "url": "/static/media/spotify-logo-round.d154494c.png"
+  }
+]);
